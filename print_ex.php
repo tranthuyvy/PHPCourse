44390ff37b22c25a490931d6f00e92fc97c3bafe
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-11-17 14:27:29 +0700

    print_ex

---
+++ print_ex.php
@@ -0,0 +1,21 @@
+<?php
+// echo "hello world 123";
+// print "hello";
+// print_r(["Thanh", "Vy", "Thúy"]);
+// var_dump(false);
+// var_export('hello');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?= "hello h1" ?></h1>
+</body>
+
+</html>
