aee2c2b3b766ed0a85dacb38acfadf5db31554b5
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-11-19 17:23:34 +0700

    cookie

---
+++ cookies.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+     Cookies: - save data in remote browser
+     - You can retrieve it when the user visit the site again
+ */
+echo "Cookies in PHP<br>";
+//save data to cookie
+setcookie('name', 'Vy', time() + 24 * 3600);
+//after 1 day, cookie will be expired 
+//1 month = 24*3600*30
+//check the existing cookie
+if (isset($_COOKIE['name'])) {
+    echo $_COOKIE['name'];
+}
+setcookie('name', '', time() - 24 * 3600); //unset cookie
